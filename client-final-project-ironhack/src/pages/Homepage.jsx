94a8286d7677ec1165de057da3b9cbb90ab889e4
Manuel Tavares <manueldiogotv@gmail.com>
2024-03-18 20:38:10 +0000

    button12

--- client-final-project-ironhack/src/pages/Homepage.jsx
+++ client-final-project-ironhack/src/pages/Homepage.jsx
@@ -18,7 +18,7 @@ function Homepage() {
                     <Text fontSize='50px' color="white">Today is <b>{day}</b>!</Text>
                     <Text pt="20px" fontSize='50px' color="white">Get A Doctor...</Text>
                     <Link to="/finddoctor" >
-                        <Button colorScheme='orange' textColor ="white" mt="20px" size='xxl' >    
+                        <Button colorScheme='orange' textColor ="white" mt="20px" width="100px">    
                         Today!
                         </Button>
                     </Link>
